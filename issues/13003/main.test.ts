c046ef1f3528a9d92c4b678415965f91a46689b1
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-12-02 15:37:23 +1100

    Update main.test.ts

--- issues/13003/main.test.ts
+++ issues/13003/main.test.ts
@@ -2,7 +2,7 @@ import { it, expect } from "bun:test";
 import * as dns from "dns";
 import { isMacos } from "../../utils";
 
-it.if(isMacos)("should return an actual IPv6 address for resolve6", async () => {
+it("should return an actual IPv6 address for resolve6", async () => {
     const domain = "example.com";
 
     expect((await dns.promises.resolve6(domain))[0]).toInclude("::ffff:")
