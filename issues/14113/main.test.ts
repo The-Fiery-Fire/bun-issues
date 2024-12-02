505ff67e48ce336b08fc8408383f49e9e40be117
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-12-02 16:37:25 +1100

    Create main.test.ts

---
+++ issues/14113/main.test.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "bun:test";
+import http from "node:http"
+
+test("node http - header with 2 values", async () => {
+
+    const res = new http.OutgoingMessage();
+    res.setHeader("myheader", ["first", "second"]);
+
+    const data = res.getHeaders()
+    
+    // it doesn't join them correctly (at least compared to node)
+    expect(data.myheader).not.toEqual(['first', 'second']);
+    expect(data.myheader).toEqual('first,second');
+
+});
