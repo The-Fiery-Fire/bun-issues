d247ae392b3eecb1ee41f65a3b482bd77285e0d5
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-12-02 16:13:04 +1100

    oops

--- issues/6854/main.test.ts
+++ issues/6854/main.test.ts
@@ -7,18 +7,18 @@ it("bun build should hoist \"use client\"", async () => {
         outdir: import.meta.dir + "\\dist", 
     })
 
-    const content = await Bun.file(import.meta.dir + "\\dist\\form.js").text()
+    const content = await Bun.file(import.meta.dir + "/dist/form.js").text()
 
     expect(content.split("\n")[0]).not.toInclude('"use client";')
 
     // even if it doesnt include react inlined its still not first
     await Bun.build({
-        entrypoints: [import.meta.dir+"\\form.tsx"],
-        outdir: import.meta.dir + "\\dist", 
+        entrypoints: [import.meta.dir+"/form.tsx"],
+        outdir: import.meta.dir + "/dist", 
         external: ['*']
     })
 
-    const content2 = await Bun.file(import.meta.dir + "\\dist\\form.js").text()
+    const content2 = await Bun.file(import.meta.dir + "/dist/form.js").text()
     expect(content2.split("\n")[0]).not.toInclude('"use client";')
 
 })
