a83370db0981c5580574502663a4c2bfd32b21ba
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-12-02 15:52:41 +1100

    Update main.test.ts

--- issues/13003/main.test.ts
+++ issues/13003/main.test.ts
@@ -1,14 +1,23 @@
 import { it, expect } from "bun:test";
 import * as dns from "dns";
-import { isMacos } from "../../utils";
+import { isMacos, isWindows } from "../../utils";
 
 it.if(isMacos)("should return an actual IPv6 address for resolve6", async () => {
     const domain = "example.com";
 
+    // on macos it gives ipv4 type ipv6 if host doesnt have ipv6
     expect((await dns.promises.resolve6(domain))[0]).toInclude("::ffff:")
 });
-it("[bonus] should fetch ipv6", async () => {
-    const res = await fetch('https://ipv6.google.com')
 
-    expect(res.ok).toBeTrue()
+it.if(isWindows)("should return an actual IPv6 address for resolve6", async () => {
+    const domain = "example.com";
+
+    // on windows it crashes if host doesnt have ipv6
+    expect(() => dns.promises.resolve6(domain)).toThrowError('Domain name not found')
 });
+
+// it("[bonus] should fetch ipv6", async () => {
+//     const res = await fetch('https://ipv6.google.com')
+
+//     expect(res.ok).toBeTrue()
+// });
