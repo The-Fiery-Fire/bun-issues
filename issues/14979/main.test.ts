358a62fa1b3e511e86d6a89f52457917a7e513c2
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-12-02 16:21:29 +1100

    more

---
+++ issues/14979/main.test.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "bun:test";
+import { spawn } from "bun";
+
+test("Panic when passing an existing/default import condition to --conditions ", async () => {
+    const process = spawn({
+        cmd: ["bun", '--conditions=import', "./index.ts"],
+        stdout: "pipe",
+        stderr: "pipe",
+        cwd: import.meta.dir
+    });
+    await process.exited
+
+    // if it crashes its not 0
+    expect(process.exitCode).not.toBe(0); 
+
+});
+
