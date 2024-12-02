7053a19b1d1b5bb706f0582473f6325fd81dc9fe
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-12-02 15:36:15 +1100

    hmm?
    i dont have macos so i test here

---
+++ issues/13003/main.test.ts
@@ -0,0 +1,9 @@
+import { it, expect } from "bun:test";
+import * as dns from "dns";
+import { isMacos } from "../../utils";
+
+it.if(isMacos)("should return an actual IPv6 address for resolve6", async () => {
+    const domain = "example.com";
+
+    expect((await dns.promises.resolve6(domain))[0]).toInclude("::ffff:")
+});
