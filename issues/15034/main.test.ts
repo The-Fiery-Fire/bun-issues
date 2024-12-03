a832fa77e72beed7bdc8fbdce4b35598261cff12
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-12-03 17:08:10 +1100

    Update main.test.ts

--- issues/15034/main.test.ts
+++ issues/15034/main.test.ts
@@ -1,19 +1,23 @@
 import net from "node:net";
 import { test, expect } from "bun:test";
 import util from "node:util"
+import { isMacos } from "../../utils";
 
 test("socket.remotePort Returns Server's Listening Port Instead of Client's Port in node:net Module", async () => {
     let remotePort = 1
     const s = net.createServer((socket) => {
         remotePort = socket.remotePort!
     })
-    s.listen(1234);
+    s.listen(3333);
 
 
-    await util.promisify(net.connect)(1234, "127.0.0.1").catch(() => {});
+    await util.promisify(net.connect)(3333, "127.0.0.1").catch(() => {});
 
     // it shouldnt be the same port
-    expect(remotePort).toBe(1234)
+    if (!isMacos) expect(remotePort).toBe(3333)
+
+    // for some reason macos makes it 1 instead
+    else expect(remotePort).toBe(1)
 
     s.close()
 });
