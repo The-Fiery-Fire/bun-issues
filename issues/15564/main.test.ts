97832395c774740ce6e7107f06919e1c64af7699
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2024-12-04 12:24:19 +1100

    more
    (this one is making these tests harder to make for windows)

---
+++ issues/15564/main.test.ts
@@ -0,0 +1,18 @@
+import { spawn } from "bun";
+import { test, expect } from "bun:test";
+import { isWindows } from "../../utils";
+
+test.if(isWindows)("bun can't read stderr from Bun.spawn", async () => {
+    const proc = spawn({
+        cmd: ["bun", "./test.ts"],
+        stderr: "pipe",
+        stdout: "pipe",
+        cwd: import.meta.dir,
+    });
+
+    const stdout = await new Response(proc.stdout).text();
+    const stderr = await new Response(proc.stderr).text();
+
+    expect(stdout).toBe("");
+    expect(stderr).not.toBe("sad");
+});
